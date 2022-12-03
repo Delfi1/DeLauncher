039b42905cbf066a8e7ecad791ca36aeb3128d9a
Delfi1 <fezdx@mail.ru>
2022-12-03 15:34:32 +0300

    Версия 0.0.1

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DeWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Установка файла с сайта:
+        public void DownloadFile(string requestString, string path){
+            HttpClient httpClient = new HttpClient();
+            var GetTask = httpClient.GetAsync(requestString);
+            GetTask.Wait(1000);
+            if (!GetTask.Result.IsSuccessStatusCode){return;}
+            using (var fs = new FileStream(path, FileMode.CreateNew)){
+                var ResponseTask = GetTask.Result.Content.CopyToAsync(fs);
+                ResponseTask.Wait(1000);
+            }
+            System.Threading.Thread.Sleep(200);
+
+        }
+        // Установка исходного кода с сайта:
+        public string DownloadStr(string requestString){
+            // Новый webClient
+            WebClient client = new WebClient();
+            Uri requestUri = new Uri(requestString);
+            string str = client.DownloadString(requestUri);
+            return str;
+        }
+
+        void InitializeWorld(){
+            Updater updater = new Updater();
+            this.Title = "De:World " + updater.Version.Content;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Application.Current.ShutdownMode = ShutdownMode.OnMainWindowClose;
+            InitializeWorld();
+        }
+
+        private void UpdateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Updater updater = new Updater();
+            updater.Show();
+        }
+    }
+}
---
+++ Updater.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DeWorld
+{
+    #pragma warning disable SYSLIB0014
+    /// <summary>
+    /// Логика взаимодействия для Updater.xaml
+    /// </summary>
+    public partial class Updater : Window
+    {
+        // Переменные:
+        string version = "0.0.1";
+        string fullPath = Environment.CurrentDirectory;
+
+        // Установка файла с сайта:
+        public void DownloadFile(string requestString, string path)
+        {
+            HttpClient httpClient = new HttpClient();
+            var GetTask = httpClient.GetAsync(requestString);
+            GetTask.Wait(1000);
+            if (!GetTask.Result.IsSuccessStatusCode) { return; }
+            using (var fs = new FileStream(path, FileMode.CreateNew))
+            {
+                var ResponseTask = GetTask.Result.Content.CopyToAsync(fs);
+                ResponseTask.Wait(1000);
+            }
+            System.Threading.Thread.Sleep(200);
+
+        }
+        // Установка исходного кода с сайта:
+        public string DownloadStr(string requestString)
+        {
+            // Новый webClient
+            WebClient client = new WebClient();
+            Uri requestUri = new Uri(requestString);
+            string str = client.DownloadString(requestUri);
+            return str;
+        }
+
+        //Проверка обновления:
+        void Check_update()
+        {
+            string get_ver = DownloadStr("https://raw.githubusercontent.com/Delfi1/DeLauncher/master/version.txt");
+            VersionServer.Content = "Server version: " + get_ver;
+            if (version.Contains(get_ver)) {UpdateBtn.IsEnabled = false;}
+            else{ UpdateBtn.IsEnabled = true; }
+        }
+
+        async void Setup_Update(){
+            await Task.Delay(100);
+            File.Move(fullPath + "\\DeWorld.exe", fullPath + "\\DeWorld_old.exe");
+            await Task.Delay(200);
+            DownloadFile(@"", fullPath + "\\DeWorld.exe");
+            await Task.Delay(100);
+            System.Diagnostics.Process.Start(fullPath + "\\DeWorld.exe");
+            Environment.Exit(0);
+        }
+
+        async void InitializeUpdater(){
+            Version.Content = "Version: " + version;
+            await Task.Delay(5000);
+            Check_update();
+
+        }
+
+        public Updater()
+        {
+            InitializeComponent();
+            InitializeUpdater();
+            
+        }
+
+        private void UpdateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Setup_Update();
+        }
+    }
+}
