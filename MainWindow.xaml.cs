bd4737ce501efb7b17510751e98a075d477db5fd
Delfi1 <fezdx@mail.ru>
2022-12-04 18:19:49 +0300

    Версия 0.2.2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -50,6 +50,7 @@ namespace DeWorld
         }
 
         public void SetupUpdate(){
+            File.Delete(gamePath + "\\Test1.pck");
             DownloadFile
                 (@"https://github.com/Delfi1/Godot_Test/blob/master/Export/Test1.pck?raw=true",
                 gamePath + "\\Test1.pck");
@@ -90,9 +91,9 @@ namespace DeWorld
                 await Task.Delay(100);
             }
             SetupUpdate();
-            await Task.Delay(100);
-            System.Diagnostics.Process.Start(gamePath + "\\Test1.exe");
             await Task.Delay(1000);
+            System.Diagnostics.Process.Start(gamePath + "\\Test1.exe");
+            await Task.Delay(100);
             Game_Btn.IsEnabled = true;
         }
     }
